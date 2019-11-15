d7488fd3f63d323699af14ea7a9426c84dfb893c
adriantelles <awesomewebdev01@gmail.com>
2019-11-16 03:44:54 +1000

    google login

--- app/modules/auth/config.js
+++ app/modules/auth/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 const GOOGLE_ID =
-  '437609978470-2bgj48j2dr3l3j677iipm56dap9glptm.apps.googleusercontent.com';
+  '437609978470-rg50f23merp83r11t42mut7cpqq1itau.apps.googleusercontent.com';
 const FACEBOOK_ID = '**************************';
 
 export { GOOGLE_ID, FACEBOOK_ID };
