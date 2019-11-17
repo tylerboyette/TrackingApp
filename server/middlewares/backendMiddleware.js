7ed60fc9dfb732059122261996181d9ca7f9dd7f
adriantelles <awesomewebdev01@gmail.com>
2019-11-18 02:13:37 +1000

    asdzcqwe

--- server/middlewares/backendMiddleware.js
+++ server/middlewares/backendMiddleware.js
@@ -11,7 +11,6 @@ module.exports = (app, cb) => {
   mongoose.set('useCreateIndex', true);
   mongoose.set('useUnifiedTopology', true);
   console.log(config.mongoURL);
-  console.log(process.env.mongoURL);
   mongoose.connect(config.mongoURL, error => {
     if (error) {
       console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
