3839bf3323ce34dbbfbfd8bcb6cd6470396b7d7e
adriantelles <awesomewebdev01@gmail.com>
2019-11-21 05:13:34 +1000

    add config

---
+++ app/constants/config.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+const GOOGLE_ID =
+  '437609978470-rg50f23merp83r11t42mut7cpqq1itau.apps.googleusercontent.com';
+const FACEBOOK_ID = '**************************';
+const MAP_KEY = 'AIzaSyAFDbvGO-bYQ1TDE8jJG3nZZAU0WnH1Abc';
+
+export { GOOGLE_ID, FACEBOOK_ID, MAP_KEY };
