d0af65fbd929645767cd31da0c38ad2893460399
adriantelles <awesomewebdev01@gmail.com>
2019-12-04 20:31:02 +1000

    fixed subscription cancel

--- server/middlewares/backendMiddleware.js
+++ server/middlewares/backendMiddleware.js
@@ -31,7 +31,7 @@ module.exports = (app, cb) => {
   app.use('/api', apiRoutes);
   app.post('/webhook', bodyParser.raw({ type: '*/*' }), (request, response) => {
     const sig = request.headers['stripe-signature'];
-    console.log(request.headers);
+    console.log('header---------', request.headers);
     let event;
 
     try {
@@ -43,6 +43,7 @@ module.exports = (app, cb) => {
     } catch (err) {
       response.status(400).send(`Webhook Error: ${err.message}`);
     }
+    console.log(event);
 
     // Handle the event
     switch (event.type) {
